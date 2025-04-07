4488201f265f9ed5db129a0b43201efab8e4b83d
Fathima-naj <fathimanajamuneer@gmail.com>
2025-04-07 19:32:57 +0530

    update dashboard navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-
-
+import Sidebar from '../component/SideNav';
+import TopNav from '../component/TopNav';
 export default function Home() {
   return (
     
-
-          <div >
-            Dashboard
-    </div>
+ <div className="flex flex-col md:flex-row bg-black md:bg-[#0D0D0D] min-h-screen text-white">
+      <Sidebar />
+      <div className="flex-1 pb-20 md:pb-0">
+        <TopNav />
+        </div>
+        </div>
   );
 }
